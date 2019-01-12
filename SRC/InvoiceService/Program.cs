7c80bf763407095b9459480f07e07968d950ad24
Eric Kirimi <ekirimi@innova.co.ke>
2019-01-12 05:47:15 +0300

    Initial project setup

---
+++ SRC/InvoiceService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InvoiceService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
