d30c1085d8e45832d4abf88796a4afaa5233fd70
Eric Kirimi <ekirimi@innova.co.ke>
2019-01-19 04:29:25 +0300

    addition of Infrastructure.ServiceDiscovery and its logic.

---
+++ SRC/Infrastructure.ServiceDiscovery/ConsulConfig.cs
@@ -0,0 +1,10 @@
+
+namespace Infrastructure.ServiceDiscovery
+{
+    public class ConsulConfig
+    {
+        public string Address { get; set; }
+        public string ServiceName { get; set; }
+        public string ServiceID { get; set; }
+    }
+}
