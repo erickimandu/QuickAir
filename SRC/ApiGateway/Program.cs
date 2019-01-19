af46ffba468a1a00a7616216dae90c8cd771fc6f
Eric Kirimi <ekirimi@innova.co.ke>
2019-01-19 03:34:16 +0300

    gateway configuration cleanup

--- SRC/ApiGateway/Program.cs
+++ SRC/ApiGateway/Program.cs
@@ -1,24 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Ocelot.DependencyInjection;
+using Serilog;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.Extensions.HealthChecks;
+using System.Threading.Tasks;
+
 
 namespace ApiGateway
 {
     public class Program
     {
+        private static IConfiguration Configuration;
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
-
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+              WebHost.CreateDefaultBuilder(args)
+                     .UseSerilog()
+                     .UseHealthChecks("/hc")
+                     .UseIISIntegration()
+                     .UseKestrel()
+                     .UseContentRoot(Directory.GetCurrentDirectory())
+                     .ConfigureAppConfiguration((hostingContext, config) =>
+                     {
+                         config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                               .AddJsonFile("appsettings.json", true, true)
+                               .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+
+                               // add ocelot configuration
+                               string ocelotConfigPath = Path.Combine(hostingContext.HostingEnvironment.ContentRootPath, "OcelotConfig");
+                               ocelotConfigPath = Path.Combine(ocelotConfigPath, hostingContext.HostingEnvironment.EnvironmentName);
+                               config.AddOcelot(ocelotConfigPath, hostingContext.HostingEnvironment);
+                               config.AddEnvironmentVariables();
+
+                               Configuration = config.Build();
+                     })
+                     .ConfigureLogging((hostingContext, logging) =>
+                     {
+                         Log.Logger = new LoggerConfiguration()
+                                                 .ReadFrom.Configuration(Configuration)
+                                                 .CreateLogger();
+                     })
+                     .UseStartup<Startup>();
     }
 }
--- SRC/ApiGateway/Startup.cs
+++ SRC/ApiGateway/Startup.cs
@@ -6,17 +6,35 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Ocelot.DependencyInjection;
+using Serilog;
+using Microsoft.Extensions.HealthChecks;
 
 namespace ApiGateway
 {
     public class Startup
     {
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-        }
+             services.AddOcelot()
+                     .AddConsul();
+             services.AddHealthChecks(checks =>
+                    {
+                        checks.WithDefaultCacheDuration(TimeSpan.FromSeconds(5));
+                        checks.AddValueTaskCheck("HTTP Endpoint", () => new
+                            ValueTask<IHealthCheckResult>(HealthCheckResult.Healthy("Ok")));
+                    });
+         }
 
+  
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -24,11 +42,12 @@ namespace ApiGateway
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else 
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseHsts();
+            }
+             app.UseHttpsRedirection();
+             app.UseOcelot().Wait();
         }
     }
 }
